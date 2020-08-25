29557995072f8a08c54334a4c3849c1cb10a5533
Joe Stein <joe@splitwise.com>
2020-08-25 14:42:36 -0400

    Add PermissionsBoundary support for app-wide IAM policy

---
+++ spec/lib/jets/resource/iam/application_role_spec.rb
@@ -0,0 +1,41 @@
+describe Jets::Resource::Iam::ApplicationRole do
+  let(:role) { Jets::Resource::Iam::ApplicationRole.new }
+  let(:managed_iam_policy) { "AWSCloudFormationReadOnlyAccess" }
+  let(:iam_policy) { nil }
+
+  before do
+    allow(Jets.config).to receive(:iam_policy).and_return(iam_policy)
+    allow(Jets.config).to receive(:managed_iam_policy).and_return(managed_iam_policy)
+    allow(Jets.config).to receive(:permissions_boundary).and_return(permissions_boundary)
+  end
+
+  describe "iam policy" do
+    context 'when a permissions boundary is set' do
+      let(:permissions_boundary) { "AWSEC2ReadOnlyAccess" }
+
+      it "expands correctly" do
+        # pp role.definition # uncomment to debug
+
+        expect(role.definition[role.definition.keys.first]).to match(
+          {type: 'AWS::IAM::Role',
+          properties: hash_including(
+            permissions_boundary: /arn:aws.*#{permissions_boundary}/,
+            managed_policy_arns: match_array(/arn:aws.*#{managed_iam_policy}/)
+          )}
+        )
+      end
+    end
+
+    context 'when a permissions boundary is not set' do
+      let(:permissions_boundary) { nil }
+
+      it "expands correctly" do
+        # pp role.definition # uncomment to debug
+
+        expect(role.definition[role.definition.keys.first][:properties]).not_to include(
+          :permissions_boundary
+        )
+      end
+    end
+  end
+end
